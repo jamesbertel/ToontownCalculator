54f440fb096b8b41082615960a2088e54d4b275c
jamesbertel <berte005@cougars.csusm.edu>
2022-01-14 16:29:23 -0800

    back front realign

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -27,72 +27,89 @@ class _HomePage extends State<HomePage> {
 
   @override
   Widget build(BuildContext context) {
+    Size size = MediaQuery
+        .of(context)
+        .size;
+
     return Scaffold(
         appBar: AppBar(
           title: const Text('Toontown Calculator'),
         ),
-        body: Center(
-            child: Column(
-                  // crossAxisAlignment: CrossAxisAlignment.center,
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[
-              //select cog type
-              Column(children: [
-                const Text('Select cog type:'),
-                DropdownButton(
-                  value: cogtype,
-                  icon: const Icon(Icons.arrow_downward),
-                  elevation: 16,
-                  style: const TextStyle(color: Colors.blue),
-                  underline: Container(
-                    height: 2,
-                    color: Colors.blueAccent,
-                  ),
-                  onChanged: (String? newValue) {
-                    setState(() {
-                      cogtype = newValue!;
-                    });
-                  },
-                  items: <String>[bossbot, lawbot, cashbot, sellbot]
-                      .map<DropdownMenuItem<String>>((String value) {
-                    return DropdownMenuItem<String>(
-                      value: value,
-                      child: Text(value),
-                    );
-                  }).toList(),
+        body: SingleChildScrollView(
+          child: Center( // WHOLE FRAME
+          child: Column(
+
+          children: <Widget>[
+
+          Container( // INPUT
+          padding: EdgeInsets.all(8.0),
+          alignment: Alignment.center,
+          width: size.shortestSide,
+          height: size.longestSide,
+          child: Column(
+        // crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.center,
+
+          children: <Widget>[
+            //select cog type
+
+            Column(children: [
+              const Text('Select cog type:'),
+              DropdownButton(
+                value: cogtype,
+                icon: const Icon(Icons.arrow_downward),
+                elevation: 16,
+                style: const TextStyle(color: Colors.blue),
+                underline: Container(
+                  height: 2,
+                  color: Colors.blueAccent,
                 ),
-              ]),
+                onChanged: (String? newValue) {
+                  setState(() {
+                    cogtype = newValue!;
+                  });
+                },
+                items: <String>[bossbot, lawbot, cashbot, sellbot]
+                    .map<DropdownMenuItem<String>>((String value) {
+                  return DropdownMenuItem<String>(
+                    value: value,
+                    child: Text(value),
+                  );
+                }).toList(),
+              ),
+            ]),
 
-              //get num obtained
-              Row(children: <Widget>[
-                const Text('Enter total # credit obtained: '),
-                Container(
-                    width: 100,
-                    height: 50,
-                    //child: Expanded(
-                    child: TextField(
-                      controller: _controller,
-                      // keyboardType: TextInputType.number,
-                      // inputFormatters: [ FilteringTextInputFormatter.digitsOnly, ],
-                      decoration: const InputDecoration(
-                        border: OutlineInputBorder(),
-                        labelText: '# Already obtained',
-                        contentPadding: EdgeInsets.symmetric(vertical: 40.0),
-                      ),
-                      onChanged: (text) {
-                        setState(() {
-                          numObtained = int.parse(text);
-                        });
-                      },
-                    ))
-                //) // Expanded
-              ]),
+            //get num obtained
+            Row(children: <Widget>[
+              const Text('Enter total # credit obtained: '),
+              Container(
+                  width: 100,
+                  height: 50,
+                  //child: Expanded(
+                  child: TextField(
+                    controller: _controller,
+                    // keyboardType: TextInputType.number,
+                    // inputFormatters: [ FilteringTextInputFormatter.digitsOnly, ],
+                    decoration: const InputDecoration(
+                      border: OutlineInputBorder(),
+                      labelText: '# Already obtained',
+                      contentPadding: EdgeInsets.symmetric(vertical: 40.0),
+                    ),
+                    onChanged: (text) {
+                      setState(() {
+                        numObtained = int.parse(text);
+                      });
+                    },
+                  ))
+            ]),
 
-              //get num needed
-              Row(children: const <Widget>[
-                const Text('Enter total # credit needed: '),
-                const Text('TEXTFIELD HERE'),
-              ]),
-            ])));
+            //get num needed
+            Row(children: const <Widget>[
+              const Text('Enter total # credit needed: '),
+              const Text('TEXTFIELD HERE'),
+            ]),
+          ]))] // END INPUT CONTAINER
+        )))
+    );
   }
 }
